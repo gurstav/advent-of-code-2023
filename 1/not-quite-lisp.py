33f3ed55dc22eece70a3a0417aef22619ae0a82f
Gustav Sollenberg <gustav@Gustavs-MBP.hemma>
2023-12-01 22:22:26 +0100

    Add not-quite-lisp

---
+++ 1/not-quite-lisp.py
@@ -0,0 +1,19 @@
+INPUT_FILE = 'input-not-quite-lisp'
+
+map = {'(': 1, ')': -1}
+
+def read_file(path):
+    with open(path) as f:
+        lines = f.read()
+    return lines
+
+def main():
+    file = read_file(INPUT_FILE)
+    parenthesis = list(file)
+    sum = 0
+    for entry in parenthesis:
+        sum += map[entry]
+    print(sum)
+
+if __name__ == '__main__':
+    main()
