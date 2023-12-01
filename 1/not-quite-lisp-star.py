15f18df64f53ce0ecd682df13cefaa18a89ef6b2
Gustav Sollenberg <gustav@Gustavs-MBP.hemma>
2023-12-01 22:35:38 +0100

    Rename files, add solution to not quite lisp star

--- 1/not-quite-lisp-star.py
+++ 1/not-quite-lisp-star.py
@@ -11,10 +11,12 @@ def main():
     file = read_file(INPUT_FILE)
     parenthesis = list(file)
     sum = 0
-    for entry in parenthesis:
+    for index, entry in enumerate(parenthesis):
         sum += map[entry]
-        
-    print(sum)
+        position = index+1
+        if (sum == -1):
+            print("Reaches the basement (floor -1) for the first time at position", position)
+            break
 
 if __name__ == '__main__':
     main()
